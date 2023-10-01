95c0c433276d3038514a88c012c91ab653163609
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-02 00:18:44 +0600

    Starting of the auth moha milon website

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='text-3xl'>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
