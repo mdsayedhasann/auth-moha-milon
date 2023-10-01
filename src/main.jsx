3be9fbff4e1061b09616af2a90440ab2cd74785f
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-02 01:31:02 +0600

    implement the basic context

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import Root from './Root/Root';
-import Home from './Home';
-import Register from './Register';
-import Login from './Login';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./Root/Root";
+import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+import AuthProvider from "./providers/AuthProvider";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home></Home>,
       },
       {
-        path: '/register',
-        element: <Register></Register>
+        path: "/register",
+        element: <Register></Register>,
       },
       {
-        path: '/login',
-        element: <Login></Login>
-      }
-    ]
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext(null)
+const AuthProvider = ({children}) => {
+
+    const authInfo = {name: 'nodi sagor khal bil'}
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+/**
+ * Create Context 
+ * Set provider with value
+ * Use the Auth provider in the main.jsx file
+ * access children in the authProvider component as children and use it in the middle of the Provider
+ * 
+ */
